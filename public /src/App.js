40c5647634d5d657a70a970ca17c784ded6e841e
Azhardes <azharaudesigns@gmail.com>
2025-04-10 14:39:56 +0530

    Add App.js file in src

---
+++ public /src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to MB Designs Portfolio</h1>
+      <p>Showcasing wedding albums and photo edits.</p>
+    </div>
+  );
+}
+
+export default App;
